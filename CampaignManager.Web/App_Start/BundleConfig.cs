0b87a3a42b55f94d42acf5805d47a7754268edff
Josh <xproj2501x@gmail.com>
2013-10-14 00:05:08 -0500

    modification to site layout

--- CampaignManager.Web/App_Start/BundleConfig.cs
+++ CampaignManager.Web/App_Start/BundleConfig.cs
@@ -8,12 +8,15 @@ namespace CampaignManager.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/knockout").Include("~/Scripts/knockout-{version}.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/Site.css"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap/bootstrap.css"));
         }
     }
 }
